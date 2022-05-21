d701f3578d2dfe54c0bea4429a528097553cc0e9
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2022-05-20 22:45:18 -0500

    Add files via upload

---
+++ EjerciciosClase/Semana05/letterHist.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void letterHist(string a){
+	
+	int letras[255];
+	
+	
+	for(int i = 0; i < 255; i++){
+		letras[i] = 0;
+	}
+	
+	for (int i = 0; i < a.length(); i++){
+		if (a[i] != '\n'){
+			letras[int(a[i])]++;
+		}
+	}
+	
+	
+	for (int i = 0; i < 255; i++) {
+		if (letras[i] != 0) {
+			cout<<(char)(i) << ": " << letras[i] << " ";
+			for (int j = 0; j < letras[i]; j++){
+				cout << "*";
+			}
+			cout << endl;
+		}
+	}
+	
+}
+
+int main(int argc, char *argv[]) {
+
+	string a;
+	
+	cout<<"\nIngrese una palabra: "; cin>>a;
+	
+	letterHist(a);
+	
+	return 0;
+}
+
