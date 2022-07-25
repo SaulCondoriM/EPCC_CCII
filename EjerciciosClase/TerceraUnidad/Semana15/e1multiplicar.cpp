74521daff90f489ad1bc7f29d03f84f535d81ce6
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2022-07-25 11:43:22 -0500

    Add files via upload

---
+++ EjerciciosClase/TerceraUnidad/Semana15/e1multiplicar.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+template<size_t X,size_t Y,size_t Z>
+
+void multiplicarMatriz(int (&M)[X][Y][Z], int n){
+	for (size_t i=0; i<X; i++){
+		
+		for (size_t j=0; j<Y; j++){
+			
+			for (size_t k=0; k<Z; k++){
+				
+				M[i][j][k] *= n;
+			}   
+		}
+	}
+}
+
+template<size_t X,size_t Y,size_t Z> 
+
+void mostrarMatriz(int (&M)[X][Y][Z]){
+	
+	for (size_t i=0; i<X; i++){
+		
+		for (size_t j=0; j<Y; j++){
+			
+			for (size_t k=0; k<Z; k++){
+				
+				cout<<M[i][j][k]<<"  ";
+			}
+			
+		}
+		cout<<endl;
+	}
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	int x[3][4][2] = {{{1,2},{4,5},{7,8},{1,2}},
+					{{10,11},{13,14},{16,17},{1,2}},
+					{{19,20},{22,23},{25,26},{1,2}}};
+	
+	
+	cout<<"-------MATRIZ------"<<endl;
+	mostrarMatriz(x);
+	cout<<endl;
+	
+	multiplicarMatriz(x,2);
+	
+	cout<<"\n-------MATRIZ(Multiplicada)------"<<endl;
+	mostrarMatriz(x);
+	
+	return 0;
+}
+
---
+++ EjerciciosClase/TerceraUnidad/Semana15/e2funcion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+float sumafi(float a, int b){
+	return a+b;
+}
+double restalf(float a, double b){
+	return a-b;
+}
+long multiplicaid(int a, long b){
+	return a*b;
+}
+	
+template<typename T, typename F, typename G>
+T operaciones(T(*op)(F,G),F x, G z){
+	return op(x,z);
+}
+	
+
+int main(int argc, char *argv[]) {
+	
+	cout<<"\nSuma"<<endl;
+	cout<<operaciones(sumafi, 8.1f, 9)<<endl;
+	cout<<operaciones(sumafi, 1.8f, 2)<<endl;
+	
+	cout<<"\nResta"<<endl;
+	cout<<operaciones(restalf, 20.1f, 15.145)<<endl;
+	cout<<operaciones(restalf, 47.9f, 34.128)<<endl;
+	
+	cout<<"\nMultiplicacion"<<endl;
+	cout<<operaciones(multiplicaid, 26, 1L)<<endl;
+	cout<<operaciones(multiplicaid, 3, 15L)<<endl;
+	
+	
+	return 0;
+}
+
